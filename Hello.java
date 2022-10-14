da5acaeb4443b43163532d93bc9b2f32f7ad7be0
uma <umamaheswari4753@gmail.com>
2022-10-14 21:21:12 +0530

    Second commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@ public class Hello
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello, World from GIT");
+		System.out.println("Hello,World from GIT");
+		System.out.println("Changes after first commit"):
+
 	}
 }
 
